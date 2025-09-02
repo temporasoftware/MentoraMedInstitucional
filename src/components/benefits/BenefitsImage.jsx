b51685a9e57ce4ae67a4d487614bdd31536bd6cb
Leonardo <leonardomullerdelima@pm.me>
2025-09-02 12:21:33 -0300

    feat: remove image animation

--- src/components/benefits/BenefitsImage.jsx
+++ src/components/benefits/BenefitsImage.jsx
@@ -2,7 +2,7 @@ import imagePath from '../../assets/images/um-dia-no-escritorio-de-uma-agencia-d
 
 const BenefitsImage = () => (
   <div className="col-lg-6">
-    <div id="sb-5-3" className="wow animate__animated animate__fadeInUp animate__delay-1">
+    <div id="sb-5-3">
       <img className="img-fluid r-20" src={imagePath} alt="content-image" />
     </div>
   </div>
